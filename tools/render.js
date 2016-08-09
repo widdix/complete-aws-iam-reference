0e684f76ba12ea3578f38f6f6b7963f4f5ec77bf
Michael Wittig <post@michaelwittig.info>
2016-08-09 11:44:58 +0200

    rendered actions

--- tools/render.js
+++ tools/render.js
@@ -1,40 +1,60 @@
-var fs = require('fs');
+var fs = require('fs-extra');
 var parser = require('./lib/parser.js');
 var Mustache = require('mustache');
+var async = require('neo-async');
 
-function renderSitemap(services) {
+var headerTemplate = fs.readFileSync('./template/header.mustache', {encoding: 'utf8'});
+var footerTemplate = fs.readFileSync('./template/footer.mustache', {encoding: 'utf8'});
+
+function renderSitemap(data) {
    var template = fs.readFileSync('./template/sitemap.mustache', {encoding: 'utf8'});
-   var output = Mustache.render(template, {services: services});
+   var output = Mustache.render(template, data);
   fs.writeFileSync('../app/sitemap.xml', output);
 }
 
 function renderIndex(services) {
    var template = fs.readFileSync('./template/index.mustache', {encoding: 'utf8'});
    var output = Mustache.render(template, {services: services}, {
-    header: fs.readFileSync('./template/header.mustache', {encoding: 'utf8'}),
-    footer: fs.readFileSync('./template/footer.mustache', {encoding: 'utf8'})
+    header: headerTemplate,
+    footer: footerTemplate
   });
   fs.writeFileSync('../app/index.html', output);
 }
 
 function renderReferenceService(rows) {
   var service = rows[0].service;
-  var template = fs.readFileSync('./template/reference.mustache', {encoding: 'utf8'});
+  var template = fs.readFileSync('./template/service.mustache', {encoding: 'utf8'});
   var output = Mustache.render(template, {service: service, rows: rows}, {
-    header: fs.readFileSync('./template/header.mustache', {encoding: 'utf8'}),
-    footer: fs.readFileSync('./template/footer.mustache', {encoding: 'utf8'})
+    header: headerTemplate,
+    footer: footerTemplate
   });
   fs.writeFileSync('../app/reference/' + service + '.html', output);
 }
 
+function renderReferenceAction(rows) {
+  var template = fs.readFileSync('./template/action.mustache', {encoding: 'utf8'});
+  fs.mkdirSync('../app/reference/' + rows[0].service);
+  rows.forEach(function(row) {
+    var output = Mustache.render(template, row, {
+      header: headerTemplate,
+      footer: footerTemplate
+    });
+
+    fs.writeFileSync('../app/reference/' + row.service + '/' + row.action + '.html', output);
+  });
+}
+
+fs.emptyDirSync('../app/reference');
+fs.copySync('./static/index.html', '../app/reference/index.html');
 var files = fs.readdirSync('../services');
-files
+var contents = files
   .map(function(file) {
     var content = fs.readFileSync('../services/' + file, {encoding: 'utf8'});
     var lines = content.replace(/[\r]/g, '').split('\n');
     return lines.map(parser.parseLine).filter((row) => row !== null);
-  })
-  .forEach((rows) => renderReferenceService(rows));
+  });
+
+contents.forEach((rows) => {renderReferenceService(rows); renderReferenceAction(rows)});
 
 renderIndex(files.map((file) => file.replace('.md', '')));
-renderSitemap(files.map((file) => file.replace('.md', '')));
+renderSitemap({services: files.map((file) => file.replace('.md', '')), contents: contents});
